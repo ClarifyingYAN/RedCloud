9ac6583d48bbb66f5e8ccb883d9bc961dd6252f7
YAN <hu_yancheng@outlook.com>
2016-10-27 15:03:56 +0800

    add a default value(5) to storage

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->string('storage');
+            $table->string('storage')->default('5');
             $table->rememberToken();
             $table->timestamps();
         });
