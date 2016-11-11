d088cc017ea23dc6998a580542b7a2d678ff5bc7
YAN <hu_yancheng@outlook.com>
2016-11-11 17:05:52 +0800

    add basename column

--- app/Models/File.php
+++ app/Models/File.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class File extends Model
 {
-    //
+    /**
+     * file table.
+     *
+     * @var string
+     */
+    protected $table = 'filesinfo';
+
+    protected $fillable = ['filename', 'type', 'path'];
+
+    protected $visible = ['filename', 'type', 'path', 'basename'];
+
 }
