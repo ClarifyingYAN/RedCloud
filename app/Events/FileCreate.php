9f61780e069734ff7566eac5c834d0dd3db4e414
YAN <hu_yancheng@outlook.com>
2016-11-13 17:01:51 +0800

    create event and listener

--- app/Events/FileCreate.php
+++ app/Events/FileCreate.php
@@ -4,29 +4,26 @@ namespace App\Events;
 
 use App\Events\Event;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use App\Models\File;
+use App\Models\User;
 
 class FileCreate extends Event
 {
     use SerializesModels;
+
+    public $file;
+    
+    public $user;
 
     /**
      * Create a new event instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($user, $file)
     {
-        //
+        $this->file = $file;
+        $this->user = $user;
     }
 
-    /**
-     * Get the channels the event should be broadcast on.
-     *
-     * @return array
-     */
-    public function broadcastOn()
-    {
-        return [];
-    }
 }
--- app/Listeners/FileCreated.php
+++ app/Listeners/FileCreated.php
@@ -5,6 +5,7 @@ namespace App\Listeners;
 use App\Events\FileCreate;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use App\Http\Controllers\FileController;
 
 class FileCreated
 {
@@ -22,10 +23,17 @@ class FileCreated
      * Handle the event.
      *
      * @param  FileCreate  $event
-     * @return void
+     * @return bool
      */
     public function handle(FileCreate $event)
     {
-        //
+        return $this->makeDirectory($event->file);
+    }
+    
+    protected function makeDirectory($path)
+    {
+        $file = new FileController();
+        
+        return $file->makeDirectory($path);
     }
 }
